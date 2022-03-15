f88311c6c203626dd2224892fab96fc09873ae4c
pc <pc@DESKTOP-E4VBSHE>
2022-03-15 19:39:59 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    private static int Hesapla()
+    {
+        Console.Write("Kisa kenari girin: ");
+        int kk = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Uzun kenari girin: ");
+        int uk=Convert.ToInt32(Console.ReadLine());
+        int alan = uk * kk;
+        return alan;
+    }
+
+    static void Main(string[] args)
+    {
+        int sonuc = Hesapla();
+        Console.WriteLine("Alan= "+sonuc);
+        Console.ReadKey();
+    }
+}
